7772a3221eb26790d97ba167b2085e4f22bcce05
KVeena01 <kethineedi.veena14@gmail.com>
2019-06-27 11:00:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8ppawy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8ppawy)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+  Myapp= Angular jss
+
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
+import from jss 
